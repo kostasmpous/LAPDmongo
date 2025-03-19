66d0e8cacf47d993525b8a8feb3a27ff01e032f3
Konstantinos Bousinis <kostasmpous@gmail.com>
2025-03-19 21:29:03 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,8 +1,6 @@
 from pymongo import MongoClient
 
-# MongoDB connection to cloud MongoAtlas
-#connection_string = "mongodb+srv://kostasmpous:C1bLHNuvmSsmZH0U@lapd.t2ysg.mongodb.net/?retryWrites=true&w=majority&appName=Lapd"
-#client = MongoClient(connection_string,tlsCAFile=certifi.where())
+
 
 #MongoDM Community localhost
 client = MongoClient("mongodb://localhost:27017/")
